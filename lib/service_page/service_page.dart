40f9f56af10732ba2f0f1e1987379e7affefaef9
gsfopxro <gsfoxpro@gmail.com>
2020-12-09 15:52:38 +0500

    + кнопки на старнице детализации

--- lib/service_page/service_page.dart
+++ lib/service_page/service_page.dart
@@ -24,43 +24,109 @@ class ServicePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            ExpansionTile(
-              initiallyExpanded: true,
-              title: Text('Данные по заявке'),
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            Expanded(
+              child: Column(
+                children: [
+                  ExpansionTile(
+                    initiallyExpanded: true,
+                    title: Text('Данные по заявке'),
                     children: [
-                      Expanded(
-                        child: Column(
+                      Padding(
+                        padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                        child: Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(this._service.customer, style: TextStyle(fontWeight: FontWeight.bold)),
-                            SizedBox(height: 8),
-                            Text('Адрес: ${this._service.customerAddress}'),
-                            SizedBox(height: 8),
-                            Text('Информация клиента: ${this._service.comment}'),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(this._service.customer, style: TextStyle(fontWeight: FontWeight.bold)),
+                                  SizedBox(height: 8),
+                                  Text('Адрес: ${this._service.customerAddress}'),
+                                  SizedBox(height: 8),
+                                  Text('Информация клиента: ${this._service.comment}'),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 8),
+                              child: PhoneButton(phone: this._service.phone),
+                            )
                           ],
                         ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 8),
-                        child: PhoneButton(phone: this._service.phone),
                       )
                     ],
                   ),
-                )
-              ],
+                  Container(
+                    child: Expanded(
+                      child: PageView(
+                        controller: controller,
+                        children: [
+                          ServiceTOPageView(title: 'Услуги ТО-1'),
+                          ServiceTOPageView(title: 'Услуги ТО-2'),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
             Container(
-              child: Expanded(
-                child: PageView(
-                  controller: controller,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(width: 1.0, color: Colors.grey),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ServiceTOPageView(title: 'Услуги ТО-1'),
-                    ServiceTOPageView(title: 'Услуги ТО-2'),
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 8),
+                            child: Icon(
+                                Icons.cancel,
+                                color: Colors.red,
+                                size: 24.0
+                            ),
+                          ),
+                          Text('Отказ')
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 8),
+                            child: Icon(
+                                Icons.calendar_today_rounded,
+                                color: Colors.blue,
+                                size: 24.0
+                            ),
+                          ),
+                          Text('Перенести дату')
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 8),
+                            child: Icon(
+                                Icons.check_circle,
+                                color: Colors.green,
+                                size: 24.0
+                            ),
+                          ),
+                          Text('Завершить')
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
