6117171a73046e97e681896ca3940f3a30ac4984
ivan.pomytkin <ivan.pomytkin@it-swarm.pro>
2021-01-25 17:29:50 +0500

    fixes

--- lib/widgets/login_page/login_page.dart
+++ lib/widgets/login_page/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:service_app/constants/app_colors.dart';
 import 'package:service_app/get/controllers/account_controller.dart';
 
 class LoginPage extends StatefulWidget {
@@ -32,43 +33,47 @@ class _LoginPageState extends State<LoginPage> {
           // in the middle of the parent.
           child: Container(
             padding: EdgeInsets.all(24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                    keyboardType: TextInputType.text,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(hintText: 'Логин'),
-                    onChanged: (text) {
-                      accountController.username.value = text;
-                    }),
-                SizedBox(
-                  height: 24.0,
-                ),
-                TextField(
-                    keyboardType: TextInputType.text,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(hintText: 'Пароль'),
-                    onChanged: (text) {
-                      accountController.password.value = text;
-                    }),
-                SizedBox(
-                  height: 36.0,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 48,
-                  child: RaisedButton(
-                      onPressed: () async {
-                        changeAuthState(true);
-                        await accountController.login();
-                        changeAuthState(false);
-                      },
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      child: Text('Войти')),
-                )
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.handyman_rounded,
+                    size: 100,
+                    color: kSecondColor,
+                  ),
+                  SizedBox(height: 40),
+                  TextField(
+                      keyboardType: TextInputType.text,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(hintText: 'Логин'),
+                      onChanged: (text) {
+                        accountController.username.value = text;
+                      }),
+                  SizedBox(height: 24.0),
+                  TextField(
+                      keyboardType: TextInputType.text,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(hintText: 'Пароль'),
+                      onChanged: (text) {
+                        accountController.password.value = text;
+                      }),
+                  SizedBox(height: 36.0),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 48,
+                    child: RaisedButton(
+                        onPressed: () async {
+                          changeAuthState(true);
+                          await accountController.login();
+                          changeAuthState(false);
+                        },
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        child: Text('Войти')),
+                  )
+                ],
+              ),
             ),
           ),
         ),
